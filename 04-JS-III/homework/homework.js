dc94f7a0dc1f38c4f54ee67a3175575906bbdbc0
Joaquin Modai <joaquin.modai@gmail.com>
2022-04-05 13:30:07 -0300

    ultimos cambios realizados

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,26 +3,35 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+ return array [0]
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+ return array [array.length -1]
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+return (array.length) ;
 }
-
 
 function incrementarPorUno(array) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Aumenta cada entero por 1
   // y devuelve el array
-  // Tu código:
+  // Tu código
+  var NuevoArray= [];
+  for(var i=0 ; i < array.length ; i++) {
+    NuevoArray[i]=array[i]+1;
+  }
+  return NuevoArray;
+ 
+ 
 }
 
 
@@ -30,6 +39,10 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+array[array.length] = elemento;
+return array;
+
+
 }
 
 
@@ -38,6 +51,9 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
+
 }
 
 
@@ -47,6 +63,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+ return palabras.join (' ');
 }
 
 
@@ -54,6 +71,9 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+for(var i = 0; i < array.length; i++){
+  
+}
 }
 
 
